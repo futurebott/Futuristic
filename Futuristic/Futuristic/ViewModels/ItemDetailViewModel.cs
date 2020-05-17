ffbbb575e1663ae0e260d7f4bdff8c4c53b61ab2
devaddict1915 <devaddict@outlook.com>
2020-05-17 14:34:15 -0400

    Add project files.

---
+++ Futuristic/Futuristic/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Futuristic.Models;
+
+namespace Futuristic.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
